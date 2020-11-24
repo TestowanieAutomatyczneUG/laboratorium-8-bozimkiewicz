fe243321c923f19d68b9511729ee33c2bc100388
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-24 23:41:15 +0100

    Added and tested methods for Meal class

--- src/meal.py
+++ src/meal.py
@@ -2,5 +2,51 @@ import requests
 
 
 class Meal:
-    def __init__(self):
-        return
+    def search_meal_by_name(self, name):
+        name_input = name.replace(' ', '_')
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/search.php?s={name_input}").json()
+        return meal['meals'][0]
+
+    def search_meal_by_id(self, id):
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/lookup.php?i={id}").json()
+        return meal['meals'][0]
+
+    def get_random_meal(self):
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/random.php").json()
+        return meal['meals'][0]
+
+    def get_meal_categories(self):
+        categories = requests.get(f"https://www.themealdb.com/api/json/v1/1/categories.php").json()
+        return categories['categories']
+
+    def get_ingredients(self):
+        ingredients = requests.get(f"https://www.themealdb.com/api/json/v1/1/list.php?i=list").json()
+        return ingredients['meals']
+
+    def get_categories(self):
+        categories = requests.get(f"https://www.themealdb.com/api/json/v1/1/list.php?c=list").json()
+        return categories['meals']
+
+    def get_area(self):
+        area = requests.get(f"https://www.themealdb.com/api/json/v1/1/list.php?a=list").json()
+        return area['meals']
+
+    def filter_by_main_ingredient(self, ingredient):
+        ingredient_input = ingredient.replace(' ', '_')
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/filter.php?i={ingredient_input}").json()
+        return meal['meals']
+
+    def filter_by_category(self, category):
+        category_input = category.replace(' ', '_')
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/filter.php?c={category_input}").json()
+        return meal['meals']
+
+    def filter_by_area(self, area):
+        area_input = area.replace(' ', '_')
+        meal = requests.get(f"https://www.themealdb.com/api/json/v1/1/filter.php?a={area_input}").json()
+        return meal['meals']
+
+
+meal = Meal()
+
+print(len(meal.get_area()))
--- tests/meal_test.py
+++ tests/meal_test.py
@@ -6,5 +6,32 @@ class TestMeal(unittest.TestCase):
     def setUp(self):
         self.temp = Meal()
 
+    def test_search_meal_by_name(self):
+        self.assertEqual(self.temp.search_meal_by_name('arrabiata')['idMeal'], '52771')
+
+    def test_search_meal_by_id(self):
+        self.assertEqual(self.temp.search_meal_by_id('52771')['strMeal'], 'Spicy Arrabiata Penne')
+
+    def test_get_meal_categories(self):
+        self.assertEqual(len(self.temp.get_meal_categories()), 14)
+
+    def test_get_ingredients(self):
+        self.assertEqual(len(self.temp.get_ingredients()), 571)
+
+    def test_get_categories(self):
+        self.assertEqual(len(self.temp.get_categories()), 14)
+
+    def test_get_area(self):
+        self.assertEqual(len(self.temp.get_area()), 25)
+
+    def test_filter_by_main_ingredient(self):
+        self.assertEqual(self.temp.filter_by_main_ingredient('chicken breast')[0]['strMeal'], 'Chick-Fil-A Sandwich')
+
+    def test_filter_by_category(self):
+        self.assertEqual(self.temp.filter_by_category('seafood')[0]['strMeal'], 'Baked salmon with fennel & tomatoes')
+
+    def test_filter_by_area(self):
+        self.assertEqual(self.temp.filter_by_area('canadian')[0]['strMeal'], 'BeaverTails')
+
     def tearDown(self):
         self.temp = None
